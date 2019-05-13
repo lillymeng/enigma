e8bc19ba00696410cd3fa36ccec28a32e10ea7d6
Lilly Meng <lillianlmeng@gmail.com>
2019-05-13 00:19:23 -0400

    header for pitchers

--- js/scenePitchers.js
+++ js/scenePitchers.js
@@ -11,8 +11,6 @@ function Pitcher(container, milk, capacity, quartsMilk, isClicked, edges) {
 }
 
 function handleClick(event) {
-  event.preventDefault();
-
   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
   mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
